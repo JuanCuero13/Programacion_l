a5a1a4608c3e85cbff53c1d4f3f49c67cceb05e5
JuanCuero13 <juanccuero@estudiante.uniajc.edu.co>
2024-09-30 21:24:03 +0000

    ExplicacionDoWhile

---
+++ Clase30sep2024/EjercicioDoWhile.java
@@ -0,0 +1,15 @@
+public class EjercicioDoWhile {
+    public static void main(String[] args) {
+
+        String cad = "Programacion I";
+        int j = 1;
+        do {
+            System.out.println(cad);
+            j++;
+        } while (j > 5);
+
+        for (int i = 1; j > 5; i++) {
+            System.out.println(cad);
+        }
+    }
+}
