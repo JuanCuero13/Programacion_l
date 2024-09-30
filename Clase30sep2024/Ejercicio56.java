341e4cbfe59e9b88c46528a8b59b77a7227075cd
JuanCuero13 <juanccuero@estudiante.uniajc.edu.co>
2024-09-30 20:38:14 +0000

    Finalizacion_ejercicio56

--- Clase30sep2024/Ejercicio56.java
+++ Clase30sep2024/Ejercicio56.java
@@ -1,5 +1,24 @@
 public class Ejercicio56 {
     public static void main(String[] args) {
-        System.out.println("Mi primer programa de JAVA en Github");
+        double nota = 0.0;
+        int aprobados = 0, noAprobados = 0;
+        //Notas de los estudiantes
+
+        Scanner leer = new Scanner(System.in);
+        Systen.out.print(s: "Digitre la cantidad de estudiantes: ");
+        int estudiantes = leer.nextInt();
+
+        for(int i = 1, i <= estudiantes; i++){
+            System.out.print("Nota del estudiante" + i + ": ");
+            nota = leer.nextDouble();
+
+            if (nota >= 5.0){
+                aprobados++;
+            }else{
+                noAprobados++;   
+        }
+    }
+    
+    System.out.println(" aprobados: + aprobados + "No Aprobados:" + noAprobados);
     }
 }        
